f8ad2652b8bfe2e2407d81f5796cdda0eacb83c8
ansatha <ansatha@hotmail.com>
2021-07-29 16:05:07 +0200

    feat(script.js): Identificación de la provincia dada una comunidad)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let reserva1 = {"name":"Juan", "lastN":"Martínez", "email":"email1@gmail.com", "from":"Madrid","to":"Canarias"};
-let reserva2 = {"name":"María", "lastN":"Sánchez", "email":"email2@gmail.com","from":"Barcelona","to": "Mayorca"};
-let reserva3 = {"name":"Ana", "lastN":"Pérez", "email":"email3@gmail.com", "from":"Palencia", "to": "Galicia"};
+let reserva1 = {"name":"Juan", "lastN":"Martínez", "email":"email1@gmail.com", "from":"Madrid","to":"Pontevedra"};
+let reserva2 = {"name":"María", "lastN":"Sánchez", "email":"email2@gmail.com","from":"Barcelona","to": "Tenerife"};
+let reserva3 = {"name":"Ana", "lastN":"Pérez", "email":"email3@gmail.com", "from":"Palencia", "to": "A Coruña"};
 let reserva4 = {"name":"John", "lastN":"Jurado", "email":"email4@gmail.com", "from":"Lepe","to":"Los Angeles"};
-let reserva5 = {"name":"Pedro", "lastN":"Johnson", "email":"email5@gmail.com", "from":"Toledo","to":"Tegucigalpa"};
-let reserva6 = {"name":"Pablo", "lastN":"Mut", "email":"email6@gmail.com", "from":"Burgos","to":"Lima"};
+let reserva5 = {"name":"Pedro", "lastN":"Johnson", "email":"email5@gmail.com", "from":"Toledo","to":"Palma"};
+let reserva6 = {"name":"Pablo", "lastN":"Mut", "email":"email6@gmail.com", "from":"Burgos","to":"Las Palmas"};
 let arrayReservas = [reserva1, reserva2, reserva3, reserva4, reserva5, reserva6];
 
 
@@ -29,9 +29,15 @@ function catcherInfo()
 function filterData()
 {
     let county = document.getElementById("destinos").value;
-    console.log(county);
-
+    let comunidadToProvincia = {'Galicia': ['Orense', 'Lugo', 'Pontevedra', 'A Coruña'],
+                                'Canarias': ['Las Palmas', 'Tenerife'],
+                                'Mallorca': ['Palma']};
     let arrayResultados = [];
-    arrayResultados.push(arrayReservas.filter(a => a.to == county))
+
+    for (let province = 0; province < comunidadToProvincia[county].length ; province++)
+    {
+        arrayResultados.push(arrayReservas.filter(a => a.to == comunidadToProvincia[county][province]));
+    };
+
     alert(JSON.stringify(arrayResultados));
-}
+};
